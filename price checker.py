7fd3de60b39022ac576e9a41fbc252d3e128ed7c
Italo Seara <54297273+italoseara@users.noreply.github.com>
2020-10-17 13:33:17 -0300

    Price checker

--- price checker.py
+++ price checker.py
@@ -11,6 +11,7 @@ password = 'your_password'
 
 price_you_want = 999.99
 
+print('Checking the price...')
 check_price()
 
 def check_price():
@@ -24,15 +25,23 @@ def check_price():
 
     price = soup.find('span', class_="price-sales")
     price_formated = float(price.get_text().strip()[3:].replace(',', '.'))
+    print(f'price: {price_formated}')
 
     if price_formated <= price_you_want:
+        print('The price is lower!')
         send_mail(tittle_formated, price_formated)
+    else:
+        print('The price isn\'t lower')
     
 def send_mail(tittle, price):
     conn = smtplib.SMTP("smtp.gmail.com", 587)
     conn.ehlo()
     conn.starttls()
     conn.ehlo()
+    print('Logging in...')
     conn.login(email, password)
+    print('Logged')
+    print('Sending email...')
     conn.sendmail('from@gmail.com', 'to@gmail.com', f'Subject: {tittle}\n\n The {tittle} is R${price}\n{URL}')
+    print('Email has been sent!')
     conn.quit()
